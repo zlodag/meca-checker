87fba2f6ddff69e09ed29ad75ca64e9400171157
zlodag <zlodag@users.noreply.github.com>
2017-01-19 20:14:16 +1300

    linted

--- app/compliance.matchers.ts
+++ app/compliance.matchers.ts
@@ -1,14 +1,12 @@
-function check72(hoursPerDay : number[]): boolean {
+function check72(hoursPerDay: number[]): boolean {
     return checkHours(7, 72, hoursPerDay);
 }
-
-function check144(hoursPerDay : number[]): boolean {
+function check144(hoursPerDay: number[]): boolean {
     return checkHours(14, 144, hoursPerDay);
 }
-
-function checkHours(periodInDays: number, maxHoursInPeriod: number, hoursPerDay : number[]): boolean {
+function checkHours(periodInDays: number, maxHoursInPeriod: number, hoursPerDay: number[]): boolean {
     let totalHours = 0;
-    for (let i = 0; i < hoursPerDay.length; i++){
+    for (let i = 0; i < hoursPerDay.length; i++) {
         totalHours += hoursPerDay[i];
         if (i >= periodInDays) {
             totalHours -= hoursPerDay[i - periodInDays];
@@ -19,13 +17,12 @@ function checkHours(periodInDays: number, maxHoursInPeriod: number, hoursPerDay 
     }
     return true;
 }
-
-function check2(hoursPerDayStartingMonday : number[]): boolean {
-    let lastWeekendWorked : number = null;
-    for (let i = 0; i < hoursPerDayStartingMonday.length; i++){
+function check2(hoursPerDayStartingMonday: number[]): boolean {
+    let lastWeekendWorked: number = null;
+    for (let i = 0; i < hoursPerDayStartingMonday.length; i++) {
         if ((i % 7 > 4) && (hoursPerDayStartingMonday[i] > 0)) {
             const thisWeekend = Math.floor(i / 7);
-            if (lastWeekendWorked === thisWeekend - 1){
+            if (lastWeekendWorked === thisWeekend - 1) {
                 return false;
             }
             lastWeekendWorked = thisWeekend;
@@ -33,47 +30,45 @@ function check2(hoursPerDayStartingMonday : number[]): boolean {
     }
     return true;
 }
-
 function check8(workAndRest: [number, number][]): boolean {
     const minRestHours = 8;
     let work: number, rest: number;
-    for (let i = 0; i < workAndRest.length; i++){
+    for (let i = 0; i < workAndRest.length; i++) {
         [work, rest] = workAndRest[i];
-        if (rest < minRestHours) return false;
+        if (rest < minRestHours) {
+            return false;
+        }
     }
     return true;
 }
-
 function check16(workAndRest: [number, number][]): boolean {
     const maxWorkHours = 16;
     let work: number, rest: number;
-    for (let i = 0; i < workAndRest.length; i++){
+    for (let i = 0; i < workAndRest.length; i++) {
         [work, rest] = workAndRest[i];
-        if (work > maxWorkHours) return false;
+        if (work > maxWorkHours) {
+            return false;
+        }
     }
     return true;
 }
-
 function getCustomMatcherResult(pass: boolean): jasmine.CustomMatcherResult {
     return {
         pass: pass,
         message: pass ? 'Should not have complied' : 'Should have complied'
     };
 }
-
-function getHoursPerDayMatcher(checkFunction: (hoursPerDayStartingMonday : number[]) => boolean): jasmine.CustomMatcher {
+function getHoursPerDayMatcher(checkFunction: (hoursPerDayStartingMonday: number[]) => boolean): jasmine.CustomMatcher {
     return {
         compare: (hoursPerDayStartingMonday: number[]) => getCustomMatcherResult(checkFunction(hoursPerDayStartingMonday))
     };
 }
-
 function getWorkAndRestMatcher(checkFunction: (workAndRest: [number, number][]) => boolean): jasmine.CustomMatcher {
     return {
         compare: (workAndRest: [number, number][]) => getCustomMatcherResult(checkFunction(workAndRest))
     };
 }
-
-export const matchers : jasmine.CustomMatcherFactories = {
+export const matchers: jasmine.CustomMatcherFactories = {
     toComplyWithLimitOnHoursPerWeek: () => getHoursPerDayMatcher(check72),
     toComplyWithLimitOnHoursPerFortnight: () => getHoursPerDayMatcher(check144),
     toComplyWithLimitOnWorkedWeekends: () => getHoursPerDayMatcher(check2),
--- app/compliance.spec.ts
+++ app/compliance.spec.ts
@@ -1,19 +1,17 @@
-/// <reference path="./compliance.matcher.d.ts"/>
+/// <reference path="./compliance.matchers.d.ts"/>
 
 import { matchers } from './compliance.matchers';
 
 beforeEach(function(){
     jasmine.addMatchers(matchers);
 });
-
 describe('Rostered hours', function(){
-    //                                 M, Tu,W, Th,F,Sa,Su, M, Tu,W, Th,F,Sa,Su, M, Tu,W, Th,F, Sa,Su
-    const compliantRoster          = [ 8, 8, 8, 8, 8,16,16, 8, 8, 8, 8, 8, 0, 0, 8, 8, 8, 8, 8, 16,16];
-    const tooManyHoursPerWeek      = [ 8, 8, 8, 8, 8, 0, 0, 8, 8, 8, 8, 8, 0, 0, 8, 8, 9, 8, 8, 16,16];
-    const tooManyHoursPerFortnight = [ 8, 8, 8, 8, 8, 0, 0, 8, 8, 8, 8, 8, 0, 0, 9, 16,16,16,16,16,16];
-    const tooManyWeekends1         = [ 8, 8, 8, 8, 8, 0, 1, 8, 8, 8, 8, 8, 1, 0, 8, 8, 8, 8, 8, 0, 0 ];
-    const tooManyWeekends2         = [ 8, 8, 8, 8, 8, 0, 0, 8, 8, 8, 8, 8, 1, 0, 8, 8, 8, 8, 8, 0, 1 ];
-
+    //                                 M, Tu,  W, Th,  F, Sa, Su,  M, Tu,  W, Th,  F, Sa, Su,  M, Tu,  W, Th,  F, Sa, Su
+    const compliantRoster          = [ 8,  8,  8,  8,  8, 16, 16,  8,  8,  8,  8,  8,  0,  0,  8,  8,  8,  8,  8, 16, 16];
+    const tooManyHoursPerWeek      = [ 8,  8,  8,  8,  8,  0,  0,  8,  8,  8,  8,  8,  0,  0,  8,  8,  9,  8,  8, 16, 16];
+    const tooManyHoursPerFortnight = [ 8,  8,  8,  8,  8,  0,  0,  8,  8,  8,  8,  8,  0,  0,  9, 16, 16, 16, 16, 16, 16];
+    const tooManyWeekends1         = [ 8,  8,  8,  8,  8,  0,  1,  8,  8,  8,  8,  8,  1,  0,  8,  8,  8,  8,  8,  0,  0];
+    const tooManyWeekends2         = [ 8,  8,  8,  8,  8,  0,  0,  8,  8,  8,  8,  8,  1,  0,  8,  8,  8,  8,  8,  0,  1];
     it('cannot exceed 72 hours per week: custom matcher', function(){
         expect(compliantRoster).toComplyWithLimitOnHoursPerWeek();
         expect(tooManyHoursPerWeek).not.toComplyWithLimitOnHoursPerWeek();
@@ -36,11 +34,10 @@ describe('Rostered hours', function(){
         expect(tooManyWeekends2).not.toComplyWithLimitOnWorkedWeekends();
     });
 });
-
 describe('Shift durations and rest periods', function(){
-    const compliantRoster = [[8,16],[8,16],[8,16],[16,8],[8,16],[0,24],[0,24]];
-    const tooMuchWork =     [[8,16],[8,16],[8,16],[17,8],[8,16],[0,24],[0,24]];
-    const notEnoughRest =   [[8,16],[8,16],[8,16],[16,7],[8,16],[0,24],[0,24]];
+    const compliantRoster = [[8, 16], [8, 16], [8, 16], [16, 8], [8, 16], [0, 24], [0, 24]];
+    const tooMuchWork     = [[8, 16], [8, 16], [8, 16], [17, 8], [8, 16], [0, 24], [0, 24]];
+    const notEnoughRest   = [[8, 16], [8, 16], [8, 16], [16, 7], [8, 16], [0, 24], [0, 24]];
     it('cannot exceed 16 hours per day', function(){
         expect(compliantRoster).toComplyWithMaximumWorkHours();
         expect(tooMuchWork).not.toComplyWithMaximumWorkHours();
@@ -52,9 +49,8 @@ describe('Shift durations and rest periods', function(){
         expect(notEnoughRest).not.toComplyWithMinimumRestHours();
     });
 });
-
 describe('Test hospital rosters', function(){
-    let hoursPerDayStartingMonday : number[];
+    let hoursPerDayStartingMonday: number[];
     let workAndRest: [number, number][];
     beforeEach(function(){
         hoursPerDayStartingMonday = undefined;
@@ -70,36 +66,36 @@ describe('Test hospital rosters', function(){
     });
     it('Sample hospital roster', function(){
         hoursPerDayStartingMonday = [
-            8.5, //M
-            8.5, //T
-            8.5, //W
-            8.5, //Th
-            8.5, //F
-            0, //Sa
-            0, //Su
-            0, //M
-            15, //T
-            8, //W        
-            8, //Th        
-            8, //Fr
-            15, //Sa
-            8, //Su
+            8.5, // M
+            8.5, // Tu
+            8.5, // W
+            8.5, // Th
+            8.5, // F
+            0,   // Sa
+            0,   // Su
+            0,   // M
+            15,  // Tu
+            8,   // W
+            8,   // Th
+            8,   // F
+            15,  // Sa
+            8,   // Su
         ];
         workAndRest = [
-            [8.5,15.5], //M
-            [8.5,15.5], //T
-            [8.5,15.5], //W
-            [8.5,15.5], //Th
-            [8.5,15.5], //F
-            [0, 24], //Sa
-            [0, 24], //Su
-            [0, 24], //M
-            [15, 9], //T
-            [8, 16], //W        
-            [8, 16], //Th        
-            [8, 16], //Fr
-            [15, 9], //Sa
-            [8, 16], //Su
+            [8.5, 15.5], // M
+            [8.5, 15.5], // T
+            [8.5, 15.5], // W
+            [8.5, 15.5], // Th
+            [8.5, 15.5], // F
+            [0, 24], // Sa
+            [0, 24], // Su
+            [0, 24], // M
+            [15, 9], // T
+            [8, 16], // W
+            [8, 16], // Th
+            [8, 16], // Fr
+            [15, 9], // Sa
+            [8, 16], // Su
         ];
     });
 });
